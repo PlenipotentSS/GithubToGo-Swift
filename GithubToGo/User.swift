745603856f82e6d09b768462e2f7c24f3751d22a
Steven Stevenson <stevenhstevenson@Stevens-MacBook-Pro.local>
2014-06-07 03:12:40 -0700

    added beginning of github user image download

--- GithubToGo/User.swift
+++ GithubToGo/User.swift
@@ -12,4 +12,16 @@ class User: NSManagedObject {
     @NSManaged var name: String?
     @NSManaged var imageURLString: String?
     @NSManaged var urlString: String?
+    let downloadQueue = NSOperationQueue()
+    
+    func downloadUserImage(url: NSURL) ->UIImage {
+        let image: UIImage?
+        downloadQueue.addOperationWithBlock({
+            //begin downloading
+            
+            
+        })
+        
+        return image!
+    }
 }
